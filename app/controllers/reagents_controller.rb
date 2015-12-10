3fc74d49efc412eea89cdfd1feb52fa848912ceb
rmholney <rmholney@gmail.com>
2015-12-10 23:07:47 +0000

    add flash code to app layout and modify contact and reagent html

--- app/controllers/reagents_controller.rb
+++ app/controllers/reagents_controller.rb
@@ -1,9 +1,23 @@
 class ReagentsController < ApplicationController
   def new
-    @contact = Reagent.new
+    @reagent = Reagent.new
   end
   
   def create
+    @reagent = Reagent.new(reagent_params)
+    
+    if @reagent.save
+      redirect_to new_reagent_path, notice: "Successfully save."
+    else
+      redirect_to new_reagent_path, notice: "Failed to save reagent."
+    end
   end
+  
+  private
+    def reagent_params
+      params.require(:reagent).permit(:reagent_name, :expiration, :flex, :flex_per_box, :test_per_flex)
+    end
+  
+  
   
 end
